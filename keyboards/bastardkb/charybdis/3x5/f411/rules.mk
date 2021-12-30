de32da575ce9a14aeaa094e2c23a26c38c9dc940
Charly Delay <0xcharly@users.noreply.github.com>
2021-12-30 23:12:26 +0900

    bastardkb/charybdis/*/f411: reenable mouse keys since the blackpill has a lot more space than the Elite-C

--- keyboards/bastardkb/charybdis/3x5/f411/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/f411/rules.mk
@@ -7,6 +7,7 @@ BOOTLOADER = stm32-dfu
 CONSOLE_ENABLE = yes
 KEYBOARD_SHARED_EP = yes
 MOUSE_SHARED_EP = no
+MOUSEKEY_ENABLE = yes # Reenable on blackpill since there's plenty of space.
 
 EEPROM_DRIVER = spi
 WS2812_DRIVER = pwm
